e7f40825b76515f297bda173deb081b4a57b7e93
Michael Ore <selbram1993@gmail.com>
2009-07-24 06:43:47 -0500

    Made game.js send controller.yaws the previous state of the images clicked.

--- erlang/www/game.js
+++ erlang/www/game.js
@@ -1,8 +1,14 @@
 var server = "/game/controller.yaws";
 function click(id) {
     try {
+	var op;
+	if ($("#" + id).attr("src")=="/www/images/x.png") {
+	    op = "clickx.png";
+	} else {
+	    op = "clicko.png";
+	}
         $.getJSON(server,
-                {'op': "click", 'id': id},
+                {'op': op, 'id': id},
                 function(data) {
                 try {
                 $("#" + data.id).attr({'src': data.src});
