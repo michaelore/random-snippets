b46b87b4950263736f84e4e0857dc8668a37e573
Michael Ore <selbram1993@gmail.com>
2009-07-24 19:09:23 -0500

    Added dictionary-server functionality to gameserver.erl

--- erlang/gameserver.erl
+++ erlang/gameserver.erl
@@ -1,6 +1,6 @@
 -module(gameserver).
 -behaviour(gen_server).
--export([start/0, introduce/0, send_update/1, stop/0, request_matrix/0]).
+-export([start/0, introduce/0, send_update/1, stop/0, request_matrix/0, store/2, lookup/1]).
 -export([init/1, handle_call/3, handle_cast/2, terminate/2]).
 
 InitMat = array:map(fun(_) -> "/www/images/x.png" end, array:new(100)),
@@ -20,23 +20,34 @@ send_update(Update) ->
 stop() ->
     gen_server:cast(gameserver, stop).
 
+store(Key, Value) ->
+    gen_server:cast(gameserver, {store, Key, Value}).
+
+lookup(Key) ->
+    gen_server:call(gameserver, {lookup, Key}).
+
 init(Mat) ->
     yaws:start_embedded("./www/"),
     sender:start(sender),
     {ok, Mat}.
 
-handle_call(matrix_please, _, Mat) ->
-    {reply, Mat}.
+handle_call(matrix_please, _, {Mat, Dict}) ->
+    {reply, Mat, {Mat, Dict}};
+handle_call({lookup, Key}, _, {Mat, Dict}) ->
+    {reply, fetch(Key, Dict), {Mat, Dict}}.
 
-handle_cast(hello, Mat) ->
+handle_cast(hello, {Mat, Dict}) ->
     sender:add(sender, From),
-    {noreply, Mat, Mat};
-handle_cast({update, Update}, Mat) ->
+    {noreply, Mat, {Mat, Dict}};
+handle_cast({update, Update}, {Mat, Dict}) ->
     sender:send(sender, {update, Update}),
-    NewMat = update(Update, Mat),
-    {noreply, NewMat};
-handle_cast(stop, Mat) ->
-    {stop, "'stop' was cast", Mat}.
+    NewMat = update(Update, {Mat, Dict}),
+    {noreply, {NewMat, Dict}};
+handle_cast({store, Key, Value}, {Mat, Dict}) ->
+    NewDict = dict:store(Key, Value, Dict),
+    {noreply, {Mat, NewDict}};
+handle_cast(stop, {Mat, Dict}) ->
+    {stop, "'stop' was cast", {Mat, Dict}}.
 
 update({Id, Image}, Mat) ->
     {X, Y} = parse_id(Id),
