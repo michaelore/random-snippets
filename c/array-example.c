da4d00affbef8ed3771fc692cfb8b7014a74073a
Michael Ore <selbram1993@gmail.com>
2009-08-06 14:51:04 -0500

    Added to array.c, and added array-example.c

---
+++ c/array-example.c
@@ -0,0 +1,13 @@
+#include "array.c"
+#include <stdio.h>
+
+int main() {
+    int statarray[6];
+    defdynamicarray(int, dynarray, 6);
+    dostatarray(int, statarray, *it = i;);
+    dodynarray(int, dynarray, *it = 5 - i;);
+    int i;
+    for(i = 0; i <= 5; i++) {
+	printf("%d\n", statarray[i] + dynarray[i]);
+    }
+}
--- c/array.c
+++ c/array.c
@@ -1,20 +1,27 @@
 // Some macros that do stuff with arrays.
 
 // Executes a piece of code once for each element of an array.
-// The element is bound to 'it' and the index is bound to 'i'.
-// Does not work with dynamic arrays at the moment.
-#define doarray(TYPE, ARRAY, CODE) \
-{ \
-    int i; \
-    TYPE it; \
-    for(i = 0; i < sizeof(ARRAY) / sizeof(TYPE); i++) { \
-	it = ARRAY[i]; \
-	CODE \
-    } \
-}
+// 'it' is a pointer to the element, and 'i' is the current index.
+// SIZE the number of elements, not the size in bytes.
+#define doarray(TYPE, ARRAY, SIZE, CODE) \
+    { \
+	int i; \
+	TYPE *it; \
+	for(i = 0, it = ARRAY; i < SIZE; i++, it +=1) { \
+	    CODE \
+	} \
+    }
 
-// Defines an array and a function that adds an element to it, adding more space to it if necessary.
+// Instance of doarray for static arrays.
+#define dostatarray(TYPE, ARRAY, CODE) doarray(TYPE, ARRAY, sizeof(ARRAY) / sizeof(TYPE), CODE)
+
+// Instance of doarray for dynamice arrays.
+#define dodynarray(TYPE, ARRAY, CODE) doarray(TYPE, ARRAY, ARRAY##_alloc, CODE)
+
+// Defines a dynamic array and a function that adds an element to it.
+// If there is not enough room to add an element, the array doubles in length.
 // The name of the function is (name_of_array)_add.
+// SIZE is the number of initial elements, not the number of bytes.
 #define defdynamicarray(TYPE, ARRAY, SIZE) \
     int ARRAY##_elems = 0; \
     int ARRAY##_alloc = SIZE; \
