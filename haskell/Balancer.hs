1daa3650af1b830de936fe7a79a02cf28a60d3aa
Michael Ore <selbram1993@gmail.com>
2009-12-19 07:19:49 -0600

    Made haskell/Balancer.hs polymorphic.

--- haskell/Balancer.hs
+++ haskell/Balancer.hs
@@ -8,8 +8,11 @@ import Data.List
 import Matrix
 import Text.ParserCombinators.Parsec
 
-balance :: String -> Maybe String
+balance :: (Monad m) => String -> m String
 balance x = parseit x >>= processit >>= balanceit >>= formatit x
+
+chemicalFail :: (Monad m) => m a
+chemicalFail = fail "This equation does not make sense."
 
 remDups :: (Eq a) => [a] -> [a]
 remDups x = remDups' [] x
@@ -26,10 +29,10 @@ replace' acc (h:t) x y = if x == h
 			   then replace' (acc ++ [y]) t x y
 			   else replace' (acc ++ [h]) t x y
 
-parseit :: String -> Maybe [[(String, Double)]]
+parseit :: (Monad m) => String -> m [[(String, Double)]]
 parseit raw = case parse chemeq "chemical equation" raw of
-		Left _ -> Nothing
-		Right x -> Just x 
+		Left _ -> chemicalFail
+		Right x -> return x 
 
 chemeq = do left <- chemex
 	    string " -> "
@@ -56,28 +59,28 @@ chempoly = do char '('
 	      num <- many1 digit <|> return "1"
 	      return ("(" ++ poly ++ ")", read num :: Double)
 
-processit :: [[(String, Double)]] -> Maybe ([String], Matrix Double)
+processit :: (Monad m) => [[(String, Double)]] -> m ([String], Matrix Double)
 processit parsed = let elems = remDups $ concat $ map (map (\x -> fst x)) parsed
 		       matrix = map (\elem -> map (countElems elem) parsed) elems
 		   in if length matrix == 0 || ((length $ head matrix) == 0) then
-                          Nothing
+                          chemicalFail
                       else if length elems == (length $ head matrix) then
-                          Just (elems, (replicate (length $ head matrix) 1):(tail matrix))
+                          return (elems, (replicate (length $ head matrix) 1):(tail matrix))
                       else if length elems + 1 == (length $ head matrix) then
-                          Just (elems, (replicate (length $ head matrix) 1):matrix)
+                          return (elems, (replicate (length $ head matrix) 1):matrix)
                       else
-                          Nothing
+                          chemicalFail
                    where countElems elem comp = sum $ map snd $ filter (\(x, _) -> x == elem) comp
 
-balanceit :: ([String], Matrix Double) -> Maybe [Int]
+balanceit :: (Monad m) => ([String], Matrix Double) -> m [Int]
 balanceit (elems, matrix) = let r = head $ transpose $ (invert matrix) * (transpose [[1] ++ (replicate ((length matrix) - 1) 0)])
 			    in makeint $ map (\x -> x / (minimum r)) r
 
-makeint :: [Double] -> Maybe [Int]
+makeint :: (Monad m) => [Double] -> m [Int]
 makeint r = makeint' r 1
-makeint' _ 1000 = Nothing
+makeint' _ 1000 = chemicalFail
 makeint' r n = if and $ map isInt result then
-		 Just $ map round result
+		 return $ map round result
 	       else
 		 makeint' r (n+1)
 	       where result = map (*n) r
@@ -85,10 +88,10 @@ makeint' r n = if and $ map isInt result then
 isInt :: Double -> Bool
 isInt n = (snd $ properFraction n) == 0
 
-formatit :: String -> [Int] -> Maybe String 
+formatit :: (Monad m) => String -> [Int] -> m String 
 formatit str res = case parse (addresult res) "" str of
-			Left _ -> Nothing
-			Right x -> Just x
+			Left _ -> chemicalFail
+			Right x -> return x
 
 addresult nums = do result <- sepBy (many1 $ oneOf $ ['a'..'z'] ++ ['A'..'Z'] ++ (map (head . show) [0..9]) ++ "()->+") space
 	            let comps = filter (\x -> not $ x == "+") $ delete "->" $ result
